4ad8e49533af423d91f47cc349760768b8fd9d3e
leelindi <46391699+leelindi@users.noreply.github.com>
2019-01-05 14:20:41 +0900

    Add files via upload

---
+++ Day5/src/window2/Dog.java
@@ -0,0 +1,25 @@
+package window2;
+
+public class Dog {
+
+//   정적특징(상태)
+//	   - 꼬리가 있음.
+//   - 다리갯수
+     boolean tail;
+     int leg;
+     
+//  동적특징(동작)
+//	   - 짖다
+//	   - 꼬리를 흔들다.
+
+    public void bark() {
+    	System.out.println("멍멍!!");
+	}
+    public void shake() {
+    	System.out.println("꼬리를 흔들다!!");
+    }
+    
+    
+    
+    
+}
